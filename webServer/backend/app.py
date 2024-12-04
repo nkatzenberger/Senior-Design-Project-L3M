cce51df3e1736d531a162ac7604c02c92533c870
bpenman <bp1023123@gmail.com>
2024-12-04 18:40:43 -0500

    web-server now serves file for download

--- webServer/backend/app.py
+++ webServer/backend/app.py
@@ -5,4 +5,4 @@ from config import app  # Import the Flask app object from config
 
 # Start the app
 if __name__ == '__main__':
-    app.run(host="localhost", port=3000, debug=True)
+    app.run(host="localhost", port=3000)
--- webServer/backend/index.py
+++ webServer/backend/index.py
@@ -1,7 +1,19 @@
-from flask import render_template
+import os
+from flask import render_template, send_file
 from config import app  # Import the Flask app object
 
 # Define the route for the home page
 @app.route('/')
 def hello_world():
     return render_template('views/index.html')
+
+@app.route('/download', methods=['GET'])
+def download_file():
+    try:
+        # Path to the file to be served
+        base_dir = os.path.abspath(os.path.dirname(__file__))  # Path to 'backend' folder
+        file_path = os.path.join(base_dir, "..", "frontend", "public", "files", "lorem.txt")  # Adjust path to reach the file
+        # send_file serves the file for download
+        return send_file(file_path, as_attachment=True)
+    except Exception as e:
+        return f"An error occurred: {e}", 500
