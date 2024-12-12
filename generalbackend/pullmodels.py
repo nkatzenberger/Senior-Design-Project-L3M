e502c33af477d8c6ff22574579ed07401dda6610
nkatzenberger <penguindude1016@gmail.com>
2024-12-11 19:12:13 -0500

    Updated Pull Model function to include links and tags

--- generalbackend/pullmodels.py
+++ generalbackend/pullmodels.py
@@ -1,11 +1,19 @@
-from huggingface_hub import HfApi, list_models #ModelSearchArguments
+from huggingface_hub import HfApi, ModelCardData #ModelSearchArguments
 api = HfApi()
 models = api.list_models()
+mdata = ModelCardData()
 #newModel = ModelSearchArguments()
 counter = 0
 for model in models: 
-    if counter <50:
-        print(model.modelId)
+    if counter <5:
+        model_info = api.model_info(model.modelId) 
+        ID = str(model.modelId)
+        splitID = ID.split('/')
+        print(f"Publisher: {splitID[0]}")
+        print(f"Model Name: {splitID[1]}") 
+        print(f"https://huggingface.co/{model.modelId}")
+        print(f"tags: {model_info.card_data.tags}\n")
         counter +=1
+        
     else: 
         break
